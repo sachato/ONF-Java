f91d66a4d00d56d0f932b81bd049cfe62520bc69
anoukvp <anoukvp1990@gmail.com>
2022-04-25 09:44:32 +0200

    navigation + incendie

--- src/ui/Incendie.java
+++ src/ui/Incendie.java
@@ -4,34 +4,35 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 
+import model.Grille;
+
 public class Incendie extends Navigation {
+	
+	private Grid grilleMoy;
+	
 	public Incendie() {
 		// Récupération des méthodes du constructeur parent (nav)
 		super();
+		setTitle("Incendie");
 		// Callback de la fonction de rajout de contenu
 		ajoutContenu();
 	}
 	
 	public void ajoutContenu() {
+		
 		// Toujours utiliser setLayout et setVisible pour la frame
 	    setLayout(null);
 	    setVisible(true);
 	    
-//	     Création d'un élément (bouton & label)
-	    JButton button = new JButton("Bouton ajouté");
-	    JLabel label = new JLabel("Mon nouveau label");
+	    //Grille
 	    
-	    // Taille d'un élément (X,Y)
-	    button.setSize(150,40); 
-	    label.setSize(150,30);
+	    grilleMoy = new Grid(new Grille ("Moyenne","25x25"));
+	    grilleMoy.setLocation(220,250);
+	    grilleMoy.setSize(1290,380);
+	    add(grilleMoy);
+	    grilleMoy.setVisible(true);
 	    
-	    // Position des éléments (X,Y)
-	    button.setLocation(100,150);
-	    label.setLocation(200,110);
-	    
-	    // & on les ajoute à la frame
-	    add(button);
-	    add(label);
+
 	}
 	
 	public static void main (String[] args) {
@@ -41,3 +42,32 @@ public class Incendie extends Navigation {
 	    content.setVisible(true);
 	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Création d'un élément (bouton & label)
+//JButton button = new JButton("Bouton ajouté");
+//JLabel label = new JLabel("Mon nouveau label");
+//
+//// Taille d'un élément (X,Y)
+//button.setSize(150,40); 
+//label.setSize(150,30);
+
+// Position des éléments (X,Y)
+//button.setLocation(100,150);
+//label.setLocation(200,110);
+
+// & on les ajoute à la frame
+//add(button);
+//add(label);
--- src/ui/Navigation.java
+++ src/ui/Navigation.java
@@ -1,10 +1,13 @@
 package ui;
 
+import ui.Incendie;
 import java.awt.Color;
+
 import java.awt.Dimension;
 import java.awt.GraphicsConfiguration;
 import java.awt.GraphicsDevice;
 import java.awt.GraphicsEnvironment;
+import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 
@@ -12,58 +15,125 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 
 public class Navigation extends JFrame implements MouseListener{
-	
+
 	public Navigation() {
-		
+
 		super("Jim Forest");
 		setResizable(false);
 		Elements();
+
 	}
-	
+
 	public void Elements() {
-		
+
 		// Création des éléments
-		
-        JButton exporterCSV = new JButton("Exporter en CSV");
-        JButton exporterBDD = new JButton("Exporter en BDD");
-        JButton simulation = new JButton("Simulation");
-        JButton init = new JButton("Initialisation");
-        JButton arbre = new JButton("Croissance des arbres");
-        JButton incendie = new JButton("Incendie");
-        JButton insecte = new JButton("Invasion d'insectes");  
-        
-        // Taille des éléments
-        
-        simulation.setSize(new Dimension(120,30));
-        init.setSize(new Dimension(120,30));
-        arbre.setSize(new Dimension(170,30));
-        incendie.setSize(new Dimension(100,30));
-        insecte.setSize(new Dimension(160,30));
-        exporterCSV.setSize(new Dimension(150,30));
-        exporterBDD.setSize(new Dimension(150,30));
-        
-        // Design des éléments
-        
-        simulation.setBackground(Color.decode("#c0d681"));  
-        init.setBackground(Color.decode("#d7e5af"));
-        arbre.setBackground(Color.decode("#d7e5af"));
-        incendie.setBackground(Color.decode("#d7e5af"));
-        insecte.setBackground(Color.decode("#d7e5af"));
-        exporterCSV.setBackground(Color.decode("#c0d681"));
-        exporterBDD.setBackground(Color.decode("#c0d681"));
-        
-        // Position des éléments
-        
-        simulation.setLocation(40,10);
-        init.setLocation(20, 75);
-        arbre.setLocation(160, 75);
-        incendie.setLocation(350, 75);
-        insecte.setLocation(470, 75);
-        exporterCSV.setLocation(810,50);
-        exporterBDD.setLocation(810,90); 
-        
-        // Ajout des éléments sur la frame
-        
+
+		JButton exporterCSV = new JButton("Exporter en CSV");
+		JButton exporterBDD = new JButton("Exporter en BDD");
+		JButton simulation = new JButton("Simulation");
+		JButton init = new JButton("Initialisation");
+		JButton arbre = new JButton("Croissance des arbres");
+		JButton incendie = new JButton("Incendie");
+		JButton insecte = new JButton("Invasion d'insectes");  
+
+		//Paramètrage des boutons
+
+
+		//Bouton Incendie
+		incendie.addActionListener(new ActionListener() {
+
+			public void actionPerformed(java.awt.event.ActionEvent e) {
+
+				setVisible(false);
+
+				Incendie incendiefenetre = new  Incendie();
+				incendiefenetre.setSize(1468, 800);
+				incendiefenetre.setVisible(true);    		
+			};
+
+		}
+				);
+		// Bouton Croissance
+
+		arbre.addActionListener(new ActionListener() {
+
+			public void actionPerformed(java.awt.event.ActionEvent e) {
+
+				setVisible(false);
+
+				Croissance arbrefenetre = new  Croissance();
+				arbrefenetre.setSize(1468, 800);
+				arbrefenetre.setVisible(true);    		
+			};
+
+		}
+				);
+
+		// Bouton Insecte
+
+		insecte.addActionListener(new ActionListener() {
+
+			public void actionPerformed(java.awt.event.ActionEvent e) {
+
+				setVisible(false);
+
+				Insecte insectefenetre = new  Insecte();
+				insectefenetre.setSize(1468, 800);
+				insectefenetre.setVisible(true);    		
+			};
+
+		}
+				);
+
+		// Bouton Initialisation
+
+		init.addActionListener(new ActionListener() {
+
+			public void actionPerformed(java.awt.event.ActionEvent e) {
+
+				setVisible(false);
+
+				Initialisation initfenetre = new  Initialisation();
+				initfenetre.setSize(1468, 800);
+				initfenetre.setVisible(true);    		
+			};
+
+		}
+				);
+
+
+		// Taille des éléments
+
+		simulation.setSize(new Dimension(120,30));
+		init.setSize(new Dimension(120,30));
+		arbre.setSize(new Dimension(170,30));
+		incendie.setSize(new Dimension(100,30));
+		insecte.setSize(new Dimension(160,30));
+		exporterCSV.setSize(new Dimension(150,30));
+		exporterBDD.setSize(new Dimension(150,30));
+
+		// Design des éléments
+
+		simulation.setBackground(Color.decode("#c0d681"));  
+		init.setBackground(Color.decode("#d7e5af"));
+		arbre.setBackground(Color.decode("#d7e5af"));
+		incendie.setBackground(Color.decode("#d7e5af"));
+		insecte.setBackground(Color.decode("#d7e5af"));
+		exporterCSV.setBackground(Color.decode("#c0d681"));
+		exporterBDD.setBackground(Color.decode("#c0d681"));
+
+		// Position des éléments
+
+		simulation.setLocation(40,10);
+		init.setLocation(20, 75);
+		arbre.setLocation(160, 75);
+		incendie.setLocation(350, 75);
+		insecte.setLocation(470, 75);
+		exporterCSV.setLocation(810,50);
+		exporterBDD.setLocation(810,90); 
+
+		// Ajout des éléments sur la frame
+
 		add(exporterCSV);
 		add(exporterBDD);
 		add(simulation);
@@ -73,32 +143,37 @@ public class Navigation extends JFrame implements MouseListener{
 		add(insecte);
 	}
 
+
+
+
+
 	public void mouseClicked(MouseEvent e) {
 		System.out.println("click");
-		
+
+
 	}
 
 	@Override
 	public void mousePressed(MouseEvent e) {
 		System.out.println("press");
-		
+
 	}
 
 	@Override
 	public void mouseReleased(MouseEvent e) {
 		System.out.println("release");
-		
+
 	}
 
 	@Override
 	public void mouseEntered(MouseEvent e) {
 		System.out.println("enter");
-		
+
 	}
 
 	@Override
 	public void mouseExited(MouseEvent e) {
 		System.out.println("bye");
-		
+
 	}
 }
